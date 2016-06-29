204dc4a9e64ce466532ba18efffd3804e75b6ff6
Sjlmejia <jmejia@ioet.com>
2016-06-29 00:29:24 -0500

    Configure seeds

--- content/themes/cima/lib/js/addArrow.js
+++ content/themes/cima/lib/js/addArrow.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
   if(window.matchMedia('(max-width: 768px)').matches){
     $( ".navbar-list .navbar-menuItem" ).hover(
       function() {
-        console.log('hola');
         $( this ).append( $( "<div class='left-arrow'></div>" ) );
       }, function() {
         $( this ).find( "div:last" ).remove();
@@ -11,7 +10,6 @@ $( document ).ready(function() {
   }else{
     $( ".navbar-list .navbar-menuItem" ).hover(
       function() {
-        console.log('hola');
         $( this ).append( $( "<div class='top-arrow'></div>" ) );
       }, function() {
         $( this ).find( "div:last" ).remove();
