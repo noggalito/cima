005014b24a36b29cd8317e42a5c0351b23986bb0
Sjlmejia <jmejia@ioet.com>
2016-08-17 21:36:00 -0500

    Fix triangle for movile_device(index)

--- content/themes/cima/lib/js/aliados.js
+++ content/themes/cima/lib/js/aliados.js
@@ -1,8 +1,19 @@
 (function($) {
   $(document).on("ready", function() {
     var pathname = window.location.pathname;
+    var widthScreen = $(window).width();
     if(pathname === "/aliados/"){
-      $(".informative-section").css("margin-top","-4px");
+      if(window.matchMedia('(max-width: 320px)').matches){
+        $(".informative-section").css("margin-top","-215px");
+        $(".main-phrase").css("margin-top","0px")
+      }else if(window.matchMedia('(max-width: 480px)').matches){
+        $(".main-phrase").css("margin-top","0px")
+        $(".informative-section").css("margin-top","-188px");
+      }else if(window.matchMedia('(max-width: 992px)').matches){
+        $(".informative-section").css("margin-top","-165px");
+      }else if(window.matchMedia('(min-width: 992px)').matches){
+        $(".informative-section").css("margin-top","-158px");
+      }
     }
   });
 })(jQuery);
